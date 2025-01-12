a079c3d18c2a8ca9660e12b183f4b3c3ec8f711a
Oleksandr <lysakova@yahoo.com>
2025-01-12 10:45:32 +0100

    fix: wrap async functions in assyncErrorCatch

--- src/entities/auth/authRouter.ts
+++ src/entities/auth/authRouter.ts
@@ -5,16 +5,17 @@ import { User } from '../users/User';
 import { UserService } from '../users/UserService';
 import { createJWT } from '../../utils/jwtFunction';
 import { userLoginSchema, userRegisterSchema } from './validation';
+import { asyncErrorCatch } from '../../utils/asyncErrorCatch';
 
 const authRouter = Router();
 const userService = new UserService(User);
 const authService = new AuthService(User, userService, createJWT);
 const authController = new AuthController(authService, userRegisterSchema, userLoginSchema);
 
-authRouter.route('/register').post((req, res, next) => authController.register(req, res, next));
+authRouter.route('/register').post(asyncErrorCatch((req, res, next) => authController.register(req, res, next)));
 
-authRouter.route('/login').post((req, res, next) => authController.login(req, res, next));
+authRouter.route('/login').post(asyncErrorCatch((req, res, next) => authController.login(req, res, next)));
 
-authRouter.route('/logout').get((req, res, next) => authController.logout(req, res, next));
+authRouter.route('/logout').get(asyncErrorCatch((req, res, next) => authController.logout(req, res, next)));
 
 export { authRouter };
