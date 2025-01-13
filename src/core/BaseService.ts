a722cb78751167462b12221d884fbd25a26452aa
Oleksandr <lysakova@yahoo.com>
2025-01-13 14:33:57 +0100

    feat: add base service class

---
+++ src/core/BaseService.ts
@@ -0,0 +1,51 @@
+import { Model } from 'mongoose';
+import { ERROR_MESSAGES, HTTP_STATUS_CODES } from '../config/constants';
+import { AppError } from './AppError';
+
+export type createProps = { data: object };
+export type FindOneByIdProps = { id: string };
+export type FindManyProps = { queryParams: object };
+export type UpdateUserProps = { data: object; id: string };
+export type DeleteUserProps = { id: string };
+
+export class BaseService<T> {
+  constructor(private model: Model<T>) {}
+
+  private checkRecordExists(record: T | null): T {
+    if (!record) {
+      throw new AppError(ERROR_MESSAGES.userNotFound, HTTP_STATUS_CODES.NOT_FOUND_404);
+    }
+
+    return record;
+  }
+
+  async create({ data }: createProps): Promise<T> {
+    const record = new this.model(data);
+
+    await record.save();
+
+    return (await this.findOneById({ id: String(record._id) })) as T;
+  }
+
+  async findOneById({ id }: FindOneByIdProps): Promise<T> {
+    const record = (await this.model.findById(id).lean()) as T;
+
+    return this.checkRecordExists(record);
+  }
+
+  async findAll({ queryParams }: FindManyProps): Promise<T[]> {
+    return await this.model.find(queryParams);
+  }
+
+  async update({ id, data }: UpdateUserProps): Promise<T> {
+    const updatedUser = (await this.model.findByIdAndUpdate(id, data, { new: true }).lean()) as T;
+
+    return this.checkRecordExists(updatedUser) as T;
+  }
+
+  async remove({ id }: DeleteUserProps): Promise<T> {
+    const deletedUser = (await this.model.findByIdAndDelete(id).lean()) as T;
+
+    return this.checkRecordExists(deletedUser);
+  }
+}
--- src/entities/users/UserService.ts
+++ src/entities/users/UserService.ts
@@ -1,55 +1,19 @@
 import { Model } from 'mongoose';
-import {
-  CreateUserProps,
-  DeleteUserProps,
-  FindManyProps,
-  FindOneByIdProps,
-  IUser,
-  UpdateUserProps,
-} from './types/types';
-import { CreateUserDto } from './dto/CreateUserDto';
-import { AppError } from '../../core/AppError';
-import { ERROR_MESSAGES, HTTP_STATUS_CODES } from '../../config/constants';
+import { CreateUserProps, IUser } from './types/types';
 
-export class UserService {
-  constructor(private User: Model<CreateUserDto>) {}
+import { BaseService } from '../../core/BaseService';
 
-  private checkUserExists(user: IUser | null): IUser {
-    if (!user) {
-      throw new AppError(ERROR_MESSAGES.userNotFound, HTTP_STATUS_CODES.NOT_FOUND_404);
-    }
-
-    return user;
+export class UserService extends BaseService<IUser> {
+  constructor(private user: Model<IUser>) {
+    super(user);
   }
 
   async create({ data }: CreateUserProps): Promise<IUser> {
-    const user = new this.User(data);
+    const user = new this.user(data);
     await user.setPassword(data.password);
 
     await user.save();
 
-    return await this.findOneById({ id: String(user._id) });
-  }
-
-  async findOneById({ id }: FindOneByIdProps): Promise<IUser> {
-    const user = await this.User.findById(id).lean();
-
-    return this.checkUserExists(user);
-  }
-
-  async findAll({ queryParams }: FindManyProps): Promise<IUser[]> {
-    return await this.User.find(queryParams);
-  }
-
-  async update({ id, data }: UpdateUserProps): Promise<IUser> {
-    const updatedUser = await this.User.findByIdAndUpdate(id, data, { new: true }).lean();
-
-    return this.checkUserExists(updatedUser);
-  }
-
-  async remove({ id }: DeleteUserProps): Promise<IUser> {
-    const deletedUser = await this.User.findByIdAndDelete(id).lean();
-
-    return this.checkUserExists(deletedUser);
+    return (await this.findOneById({ id: String(user._id) })) as IUser;
   }
 }
