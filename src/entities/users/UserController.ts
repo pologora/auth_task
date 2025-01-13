2b37bfb5181d3df0a2e51c7e32ff8a6dc27ae4d3
Oleksandr <lysakova@yahoo.com>
2025-01-13 15:21:36 +0100

    fix: get entity name from object

--- src/entities/users/UserController.ts
+++ src/entities/users/UserController.ts
@@ -2,9 +2,10 @@ import { BaseController } from '../../core/BaseController';
 import { UserService } from './UserService';
 import { Schema } from 'joi';
 import { IUser } from './types/types';
+import { ENTITIES_NAMES } from '../../config/constants';
 
 export class UserController extends BaseController<IUser> {
   constructor(userService: UserService, userCreateSchema: Schema, userUpdateSchema: Schema, userParamsSchema: Schema) {
-    super(userService, userCreateSchema, userUpdateSchema, userParamsSchema, 'User');
+    super(userService, userCreateSchema, userUpdateSchema, userParamsSchema, ENTITIES_NAMES.user);
   }
 }
